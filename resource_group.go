2c4332a9d5c4e7bdea2aa51794610fa49de5bd2f
rdarst <45427115+rdarst@users.noreply.github.com>
2018-12-12 19:00:14 -0500

    Added the ability to use names for read function

---
+++ resource_group.go
@@ -0,0 +1,136 @@
+package chkp
+
+import (
+        "github.com/hashicorp/terraform/helper/schema"
+        "./client"
+    		"encoding/json"
+)
+
+
+func resourceGroup() *schema.Resource {
+        return &schema.Resource{
+                Create: resourceGroupCreate,
+                Read:   resourceGroupRead,
+                Update: resourceGroupUpdate,
+                Delete: resourceGroupDelete,
+
+                Schema: map[string]*schema.Schema{
+
+						            "name": {
+                                Type:     schema.TypeString,
+                                Required: true,
+                        },
+                        "color": {
+                                Type:     schema.TypeString,
+                                Optional: true,
+                                Default: "black",
+                        },
+                        "members": &schema.Schema{
+                                Type:     schema.TypeSet,
+                                Optional: true,
+                                Elem: &schema.Schema{Type: schema.TypeString},
+                        },
+                        "uid": {
+                                Type:     schema.TypeString,
+                                Optional: true,
+                                Computed: true,
+                            },
+
+                },
+        }
+}
+
+func resourceGroupCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*chkp.Client)
+	var group = chkp.Group{}
+	group.Name = d.Get("name").(string)
+  group.Color = d.Get("color").(string)
+
+  // Pull in the list of Group Members
+  members := d.Get("members").(*schema.Set).List()
+  memberlist := make([]string, 0, len(members))
+	for _, v := range members {
+		val, ok := v.(string)
+		if ok && val != "" {
+			memberlist = append(memberlist, v.(string))
+		}
+  }
+  group.Members = memberlist
+
+  id, err := client.CreateGroup(group)
+  readGroup := chkp.Group{}
+  json.Unmarshal(id, &readGroup)
+	d.SetId(readGroup.Uid)
+  d.Set("uid", readGroup.Uid)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func resourceGroupRead(d *schema.ResourceData, meta interface{}) error {
+        client := meta.(*chkp.Client)
+  // Call the API to get Group info
+  id, err := client.ShowGroup(d.Id())
+
+	readGroup := chkp.Group{}
+  json.Unmarshal(id, &readGroup)
+	d.SetId(readGroup.Uid)
+	d.Set("color", readGroup.Color)
+	d.Set("name", readGroup.Name)
+  var group chkp.GroupMembers
+  json.Unmarshal(id, &group)
+
+  // Read the list of Current Group Members
+  i := 0
+  mem := make([]string, 0, len(group.Members))
+  for _, member := range readGroup.Members {
+    mem = append(mem, group.Members[i].Name)
+    _ = member
+    i += 1
+  }
+
+  d.Set("members", mem)
+  //d.Set("members").(*schema.Set).List()
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func resourceGroupUpdate(d *schema.ResourceData, meta interface{}) error {
+  client := meta.(*chkp.Client)
+	var group = chkp.Group{}
+	group.Color = d.Get("color").(string)
+  if d.HasChange("name") {group.Newname = d.Get("name").(string)}
+	group.Uid = d.Get("uid").(string)
+  // Pull in the list of Group Members
+  members := d.Get("members").(*schema.Set).List()
+  memberlist := make([]string, 0, len(members))
+  for _, v := range members {
+    val, ok := v.(string)
+    if ok && val != "" {
+      memberlist = append(memberlist, v.(string))
+    }
+  }
+  group.Members = memberlist
+
+	id, err := client.SetGroup(group)
+  readGroup := chkp.GroupMembers{}
+  if err := json.Unmarshal(id, &readGroup); err != nil {return err}
+  //json.Unmarshal(id, &readGroup)
+	d.SetId(readGroup.Uid)
+  d.Set("uid", readGroup.Uid)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func resourceGroupDelete(d *schema.ResourceData, meta interface{}) error {
+    client := meta.(*chkp.Client)
+	client.DeleteGroup(d.Id())
+	return nil
+
+}
