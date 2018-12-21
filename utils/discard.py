d3f2c14b72411f69f6655782f5c3c59f22f714a5
rdarst <45427115+rdarst@users.noreply.github.com>
2018-12-21 12:05:24 -0500

    Add files via upload

---
+++ utils/discard.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import getopt
+import requests
+import sys
+import os
+
+
+
+# Set login info
+url = os.environ['CHKP_SERVER'] + "/discard" 
+payload = "{}"
+headers = {
+    'Content-Type': "application/json",
+    'Cache-Control': "no-cache",
+    'X-chkp-sid': "" + os.environ['CHKP_SID'] + "",
+    }
+
+# SSL Certificate Checking is disabled!!!
+requests.packages.urllib3.disable_warnings()
+response = requests.request("POST", url, data=payload, headers=headers, verify=False)
+
+#Retrun the response
+print(response.text)
+
+
