4e8649b73724d1aa01117a85a470ec330322c4b3
rdarst <45427115+rdarst@users.noreply.github.com>
2018-12-21 12:43:19 -0500

    Delete applicationsite.tf

--- applicationsite.tf
+++
@@ -1,8 +0,0 @@
-resource "chkp_applicationsite" "myapplicationsite" {
-      name = "My_Terraform_ApplicationSite"
-      color = "light green"
-      urllist = ["www.google.com",  "www.ebay.com", "www.amazon.com", "www.core.com"]
-      primarycategory = "Social Networking"
-      additionalcategories = ["Instant Chat", "Supports Streaming"]
-      #tags = ["MyTagTest","MyOtherTag"]
-}
