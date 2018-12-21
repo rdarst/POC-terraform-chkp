fea556e5823a7548409b5c38fa0955a42713db63
rdarst <45427115+rdarst@users.noreply.github.com>
2018-12-21 12:41:59 -0500

    Delete tag.tf

--- tag.tf
+++
@@ -1,9 +0,0 @@
-resource "chkp_tag" "TerraformTag1" {
-      name = "MyTerraformTag1"
-      color = "blue"
-        }
-
-resource "chkp_tag" "TerraformTag2" {
-      name = "MyTerraformTag2"
-      color = "light green"
-        }
