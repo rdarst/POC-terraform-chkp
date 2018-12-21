0e76a5609062bfeb1ad033855d47c07b17d7ba05
rdarst <45427115+rdarst@users.noreply.github.com>
2018-12-21 12:42:56 -0500

    Delete group.tf

--- group.tf
+++
@@ -1,5 +0,0 @@
-resource "chkp_group" "mygroup" {
-      name = "My_Terraform_Group"
-      color = "light green"
-      members = ["${chkp_network.vnet_local.name}",  "${chkp_host.jumphost.name}"]
-}
