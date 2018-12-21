6ab7d93003bc8f7696d7122887ec3348de0db6d8
rdarst <45427115+rdarst@users.noreply.github.com>
2018-12-21 12:43:03 -0500

    Delete dynamicobject.tf

--- dynamicobject.tf
+++
@@ -1,9 +0,0 @@
-resource "chkp_dyanmicobject" "externalgateway" {
-      name = "testdynamicobject"
-      color = "blue"
-        }
-
-resource "chkp_dyanmicobject" "internalgateway" {
-      name = "internaldynamicobject"
-      color = "blue"
-        }
